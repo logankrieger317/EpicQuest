0979d223eadf3bb5d16df898b4df3e32f522d5c8
Logan <logankrieger317@gmail.com>
2023-09-21 16:00:11 -0500

    testing ability to call API

---
+++ script.js
@@ -0,0 +1,45 @@
+let button = document.querySelector("#searchButton")
+let button2 = document.querySelector("#searchButton2")
+
+button.addEventListener('click', async () => {
+    //variables 
+    let pokemonName = document.querySelector("#pokemonName")
+    let pokemonImage = document.querySelector("#pokemonImage")
+    let textInput = document.querySelector("#inputBar").value
+    //Checking to ensure user input is logged. 
+    console.log(textInput)
+    
+    //API Call
+
+    const pokeName = await axios.get(`https://api.pokemontcg.io/v2/cards/${textInput}`)
+    console.log(pokeName)
+
+  
+    //Display based on user input
+
+   
+    
+    pokemonName.innerText = pokeName.data.data.name
+    pokemonImage.src = pokeName.data.data.images.small
+
+}
+)
+
+button2.addEventListener('click', async () => {
+    //variables 
+    let pokemonName2= document.querySelector("#pokemonName2") 
+    let pokemonImage2 = document.querySelector("#pokemonImage2")
+    let textInput2 = document.querySelector("#inputBar2").value
+    let textInput = document.querySelector("#inputBar").value
+    //Checking to ensure user input is logged. 
+    console.log(textInput)
+
+
+const pokeName2 = await axios.get(`https://api.pokemontcg.io/v2/cards/${textInput2}`)
+console.log(pokeName2)
+
+    pokemonName2.innerText = pokeName2.data.data.name
+    pokemonImage2.src = pokeName2.data.data.images.small
+
+}
+)
