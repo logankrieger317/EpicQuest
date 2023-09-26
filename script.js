604cbd33985a63a0fe56e6df01d9f2203fe9c876
Logan <logankrieger317@gmail.com>
2023-09-26 10:08:58 -0500

    2nd Event Listener and HTML id Updates

--- script.js
+++ script.js
@@ -2,11 +2,17 @@ let button = document.querySelector("#startButton")
 
 
 button.addEventListener('click', async () => {
+   let allPlayerCards=  ""
+   let allComputerCards = []
+   
+    //Card Array
+  
+
+
     //Player Variables
     let pokemonName = document.querySelector("#pokemonName")
     let pokemonImage = document.querySelector("#pokemonImage")
-    let pokeMove1 = document.querySelector("#move1")
-    let pokeMove2 = document.querySelector("#move2")
+    let attackButton = document.querySelector("#attackButton")
     //Computer Variables
     let comPokemonName= document.querySelector("#comPokemonName") 
     let comPokemonImage = document.querySelector("#comPokemonImage")
@@ -14,38 +20,46 @@ button.addEventListener('click', async () => {
     
     //API Call
     const randomCard1 =  await axios.get(`https://api.pokemontcg.io/v2/cards`)
-    console.log(randomCard1.data.data)
+    allPlayerCards = randomCard1.data.data
+    console.log(allPlayerCards)
+   
+    
    
     //Random Assingment of Cards
 
-   let number = Math.floor(Math.random() * 250)
-//    let number2 = Math.floor(Math.random() * 99)
-   let number3 = Math.floor(Math.random() * 250)
-//    let number4 = Math.floor(Math.random() * 99)
+    let number1 = Math.floor(Math.random() * 250)
+    let number2 = Math.floor(Math.random() * 250)
+    let computerNum1 = Math.floor(Math.random() * 250)
+    let computerNum2 = Math.floor(Math.random() * 250)
 
-    console.log(number)
-    // console.log(number2)
+    console.log(number1)
+    console.log(number2)
 
-    //Player Cards
-    pokemonName.innerText = randomCard1.data.data[number].name
-    pokemonImage.src = randomCard1.data.data[number].images.small
-    console.log(number,)
-    
-    
-    pokeMove1.innerHTML = randomCard1.data.data[number].attacks[0].name
-    console.log(number)
-    console.log(pokeMove1)
-
-    // pokeMove2.innerHTML= randomCard1.data.data[number].attacks[1].name
-    // console.log(number,)
-    // console.log(pokeMove2)
-
+//     //Player Cards
+    pokemonName.innerText = allPlayerCards[number1].name
+    pokemonImage.src = allPlayerCards[number1].images.small
    
-   //Computer Cards
+    console.log(number1)
+    
+    
+    let attack1 =  allPlayerCards[number1].attacks[0].name
+    console.log(attack1)
+    
+
+    attackButton.innerText = attack1
+    
+    attackButton.addEventListener('click', () => {
+
+    })
+  
+
+
+
+//     //Computer Cards
     const computerCard1 = await axios.get(`https://api.pokemontcg.io/v2/cards`)
-    console.log(computerCard1.data.data)
-    comPokemonName.innerText = computerCard1.data.data[number3].name
-    comPokemonImage.src = computerCard1.data.data[number3].images.small
+    allComputerCards =computerCard1.data.data
+    comPokemonName.innerText = allComputerCards[computerNum1].name
+    comPokemonImage.src = allComputerCards[computerNum1].images.small
 
 }
 )
